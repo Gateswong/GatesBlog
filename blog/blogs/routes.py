be4643c7cb3d0871502e436cc9821dba1960d549
Gates_ice <gates@gatesice.com>
2015-04-26 03:22:59 -0500

    Use template inheritance

--- blog/blogs/routes.py
+++ blog/blogs/routes.py
@@ -4,4 +4,5 @@ from . import blogs
 
 @blogs.route("/")
 def home():
-    return render_template("blogs/index.html")
+    return render_template("blogs/index.html",
+                           site={"Title": "My Blog", "Brand": "My Blog"})
